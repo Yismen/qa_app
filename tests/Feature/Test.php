a22f6a31d47f445f521bb2968639eb083962b035
Yismen Jorge <yismen.jorge@gmail.com>
2020-08-22 10:12:21 -0400

    Initial Commit

---
+++ tests/Feature/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dainsys\QAApp\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function first()
+    {
+        $this->assertTrue(true);
+    }
+}
